95a7717c9ff1e5f52d311ef18f38e4ebbe4e5d3a
Audrey Andoy <audreylandoy@gmail.com>
2021-10-14 10:12:56 -0700

    nice neat functions

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 import requests
 
-
-def get_recipe_list(cuisine, diet):
-    PATH = "https://api.spoonacular.com/recipes/complexSearch"
-    API_KEY = "078202005f0c4419b280d63f1f5f4ca8"
-
-    query_params = {
-        "cuisine": cuisine,
-        "diet": diet,
-        "addRecipeInformation": True,
-        "fillIngredients": True,
-        "apiKey": API_KEY
-    }
-    response = requests.get(PATH, params=query_params)
-    return response.json()["results"]
-
-
-import requests
-
 def get_recipe_list(cuisine, diet):
     PATH = "https://api.spoonacular.com/recipes/complexSearch"
     API_KEY = "078202005f0c4419b280d63f1f5f4ca8"
@@ -32,26 +14,33 @@ def get_recipe_list(cuisine, diet):
     response = requests.get(PATH, params = query_params)
     return response.json()["results"]
 
-# get all recipes
-recipes = get_recipe_list("Japanese", "vegetarian")
-
-# get individual recipes
-for recipe in recipes:
+def display_title(recipe):
     print("="*40)
     print(f"             {recipe['title'].upper()}             ")
     print("="*40)
 
-    # get individual ingredients
+def display_ingredients(recipe):
     print("\nIngredients: ")
     print("------------")
     ingredients = recipe["extendedIngredients"]
     for ingredient in ingredients:
         print(ingredient["original"])
-    
-    # get individual recipe steps
+
+def display_recipe_steps(recipe):
     print("\nRecipe:")
     print("-------")
     recipe_steps = recipe["analyzedInstructions"][0]["steps"]
     for step in recipe_steps:
         print(f"#{step['number']} {step['step']} \n")
 
+def main():
+    recipes = get_recipe_list("Japanese", "vegetarian")
+
+    for recipe in recipes:
+        display_title(recipe)
+        display_ingredients(recipe)
+        display_recipe_steps(recipe)
+
+
+if __name__ == "__main__":
+    main()
