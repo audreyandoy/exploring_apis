dcc26d834c6d25b2014873aa2f7d49d58a4cbb2f
Audrey Andoy <audreylandoy@gmail.com>
2021-10-14 10:08:08 -0700

    finished

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from recipe import Recipe
 import requests
 
 
@@ -17,39 +16,42 @@ def get_recipe_list(cuisine, diet):
     return response.json()["results"]
 
 
-# def display_title(recipe):
-#     print("="*len(recipe['title']))
-#     print(f"{recipe['title'].upper()}")
-#     print("="*len(recipe['title']))
+import requests
 
+def get_recipe_list(cuisine, diet):
+    PATH = "https://api.spoonacular.com/recipes/complexSearch"
+    API_KEY = "078202005f0c4419b280d63f1f5f4ca8"
 
-# def display_ingredients(recipe):
-#     print("\nIngredients: ")
-#     print("------------")
-#     ingredients = recipe["extendedIngredients"]
-#     for ingredient in ingredients:
-#         print(ingredient["original"])
+    query_params = {
+        "cuisine": cuisine,
+        "diet": diet,
+        "addRecipeInformation": True,
+        "fillIngredients": True, 
+        "apiKey": API_KEY 
+    }
+    response = requests.get(PATH, params = query_params)
+    return response.json()["results"]
 
+# get all recipes
+recipes = get_recipe_list("Japanese", "vegetarian")
 
-# def display_recipe_steps(recipe):
-#     print("\nRecipe:")
-#     print("-------")
-#     recipe_steps = recipe["analyzedInstructions"][0]["steps"]
-#     for step in recipe_steps:
-#         print(f"#{step['number']} {step['step']} \n")
+# get individual recipes
+for recipe in recipes:
+    print("="*40)
+    print(f"             {recipe['title'].upper()}             ")
+    print("="*40)
 
+    # get individual ingredients
+    print("\nIngredients: ")
+    print("------------")
+    ingredients = recipe["extendedIngredients"]
+    for ingredient in ingredients:
+        print(ingredient["original"])
+    
+    # get individual recipe steps
+    print("\nRecipe:")
+    print("-------")
+    recipe_steps = recipe["analyzedInstructions"][0]["steps"]
+    for step in recipe_steps:
+        print(f"#{step['number']} {step['step']} \n")
 
-def display_individual_recipe_info(recipes):
-    for recipe in recipes:
-        recipe_info = Recipe(recipe)
-        recipe_info.display_title()
-        recipe_info.display_ingredients()
-        recipe_info.display_recipe_steps()
-
-def main():
-    recipes = get_recipe_list("Japanese", "vegetarian")
-    display_individual_recipe_info(recipes)
-
-
-if __name__ == "__main__":
-    main()
